5fdffbd2b9424393f168aa85fab0e777999bf7a5
Daeun <78911818+ekdms5566@users.noreply.github.com>
2022-11-08 12:02:10 +0900

    Add files via upload

---
+++ timer/src/js/main.js
@@ -0,0 +1,34 @@
+const btnStart = document.querySelector(".btn-start");
+const btnPause = document.querySelector(".btn-pause");
+const btnReset = document.querySelector(".btn-reset");
+const hours = document.querySelector("#hours");
+const minutes = document.querySelector("#minutes");
+const second = document.querySelector("#second");
+
+let time = 0;
+
+function setZero() {
+  if (!hours.value && !minutes.value && !second.value) {
+    alert("시간 입력");
+  }
+  if (!hours.value) hours.value === "00";
+  if (!minutes.value) minutes.value === "00";
+  if (!second.value) second.value === "00";
+  setTime();
+}
+
+function setTime() {
+  time =
+    parseInt(hours.value) * 60 * 60 +
+    parseInt(minutes.value) * 60 +
+    parseInt(second.value);
+  //   console.log(time);
+}
+
+function pauseTimer() {}
+
+function resetTimer() {}
+
+btnStart.addEventListener("click", setZero);
+btnPause.addEventListener("click", pauseTimer);
+btnReset.addEventListener("click", resetTimer);
